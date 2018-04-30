9727f19803ade2f47ba98372bb828ea185518a52
nosteiner <37960334+nosteiner@users.noreply.github.com>
2018-04-30 10:15:11 +0300

    fix bugs added validation to fields

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 
 var fruitsArr = [];
-var ratingNow;
+var ratingNow="";
 
 
 function addFruit(name, category, rating) {
-    var fruit = {
-        fruitName: name,
-        fruitCategory: category,
-        fruitRating: rating
-    };
-    fruitsArr.push(fruit);
+    //validation
+    if(name===""){
+        alert("please fill the name feild")        
+    }
+    if(category===""){
+        alert("please fill the category feild")        
+    }
+    if(rating===""){
+        alert("please rate the fruit")        
+    }
+//if valid create and add to array
+    if(name!==""&&category!==""&&rating!==""){
+        var fruit = {
+            fruitName: name,
+            fruitCategory: category,
+            fruitRating: rating
+        };
+        fruitsArr.push(fruit);
+    }
 }
 
 function renderFruits() {
     $(".fruits-list").empty();
 
     for (i = 0; i < fruitsArr.length; i++) {
-        var $fruitName = fruitsArr[i];
-        var $fruitCategory = fruitsArr[i];
-        var $fruitRating = fruitsArr[i];
-        $('.fruits-list').append($('<li></li>').append($fruitName["fruitName"], ", ", "rating: " + $fruitRating["fruitRating"]));
+        var $fruitData = fruitsArr[i];
+        $('.fruits-list').append($('<li></li>').append($fruitData["fruitName"], $fruitData["fruitCategory"], "rating: " + $fruitData["fruitRating"]));
     }
 }
 
 $('.star').on("click", function () {
-    var temp = $(this).attr('id')
-    ratingNow = temp;
+    var starId = $(this).attr('id') //get the rate from id
+    ratingNow = starId;
     
     $(".star").css("color", "black").html('☆')
     
-    for (i = temp; i > 0; i--) {
+    for (i = starId; i > 0; i--) {
         $("#"+i).css("color", "gold").html('★');
     }
+    
 })
 
 
@@ -46,14 +58,8 @@ var clicked = function () {
     renderFruits();
     //clear stars
     $(".star").css("color", "black").html('☆');;
-    ratingNow = 0;
-
+    ratingNow = ""; // reset rating to invalid value
 }
+
 $('button').click(clicked);
-
-
-// for(i=1;i<6;i++){
-//     document.getElementById(i).style.color = "red";
-// }
-
 
